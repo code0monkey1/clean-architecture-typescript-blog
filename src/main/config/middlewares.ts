6c3d4e1cb0d5962076e56bc8542f50e84f0e794e
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-19 16:12:41 +0000

    19 Sept 2023, 21:42

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,10 +1,10 @@
-import { Express, json } from 'express';
-// import { bodyParser } from '@main/middlewares/body-parser';
+import { bodyParser } from '@main/middlewares/body-parser';
 import { contentType } from '@main/middlewares/content-type';
 import { cors } from '@main/middlewares/cors';
+import { Express } from 'express';
 
 export default (app: Express): void => {
-  app.use(json());
+  app.use(bodyParser);
   app.use(cors);
   app.use(contentType);
 };
