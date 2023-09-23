f9f54cbdcbdb6da0ca56dca8ca419aa28259bde5
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-23 15:45:59 +0000

    23 Sept 2023, 21:15

--- src/main/routes/authentication-routes.ts
+++ src/main/routes/authentication-routes.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';
 import { expressRouteAdapter } from '@main/adapters/express-route-adapter';
 import { makeSignInController } from '@main/factories/controllers/authentication/sign-in/controller-factory';
 import { makeSignUpController } from '@main/factories/controllers/authentication/sign-up/controller-factory';
+import { Router } from 'express';
 
 export default (router: Router): void => {
   router.post('/login', expressRouteAdapter(makeSignInController()));
