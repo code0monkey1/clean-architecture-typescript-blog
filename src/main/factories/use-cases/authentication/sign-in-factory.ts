fcfa6d529fe77b3c83d3a5e4b85db62ae43967a7
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-10 11:54:00 +0000

    10 Feb 2024, 17:24

--- src/main/factories/use-cases/authentication/sign-in-factory.ts
+++ src/main/factories/use-cases/authentication/sign-in-factory.ts
@@ -1,8 +1,8 @@
 import { SignInInterface } from '@application/interfaces/use-cases/authentication/SignInInterface';
 import { SignIn } from '@application/use-cases/authentication/SignIn';
-import { UserRepository } from '@infra/db/mongodb/repositories/UserRepository';
 import { BcryptAdapter } from '@infra/cryptography/BcryptAdapter';
 import { JWTAdapter } from '@infra/cryptography/JWTAdapter';
+import { UserRepository } from '@infra/db/mongodb/repositories/UserRepository';
 import env from '@main/config/env';
 
 export const makeSignIn = (): SignInInterface => {
