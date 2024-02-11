5a16d4d3aa7bc51c1fc3a2c80dff622685bae1b7
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-11 06:47:38 +0000

    11 Feb 2024, 12:17

--- src/main/factories/controllers/comments/create-comment/controller-factory.ts
+++ src/main/factories/controllers/comments/create-comment/controller-factory.ts
@@ -1,8 +1,8 @@
 import { BaseController } from '@infra/http/controllers/BaseController';
 import { CreateCommentController } from '@infra/http/controllers/comments/CreateCommentController';
 import { makeCreateCommentValidation } from '@main/factories/controllers/comments/create-comment/validation-factory';
-import { makeGetPostById } from '@main/factories/use-cases/posts/get-post-by-id-factory';
 import { makeCreateComment } from '@main/factories/use-cases/comments/create-comment-factory';
+import { makeGetPostById } from '@main/factories/use-cases/posts/get-post-by-id-factory';
 import { makeUpdatePostTotalComments } from '@main/factories/use-cases/posts/update-post-total-comments-factory';
 
 export const makeCreateCommentController = (): BaseController => {
@@ -10,6 +10,7 @@ export const makeCreateCommentController = (): BaseController => {
   const getPostByIdUseCase = makeGetPostById();
   const createCommentUseCase = makeCreateComment();
   const updatePostTotalCommentsUseCase = makeUpdatePostTotalComments();
+
   return new CreateCommentController(
     validation,
     getPostByIdUseCase,
