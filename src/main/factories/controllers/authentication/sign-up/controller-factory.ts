4837ad84b6e8a6f2d9a776ccc064d10cb652ea13
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-27 05:25:45 +0000

    27 Feb 2024, 10:55

--- src/main/factories/controllers/authentication/sign-up/controller-factory.ts
+++ src/main/factories/controllers/authentication/sign-up/controller-factory.ts
@@ -1,8 +1,8 @@
 import { BaseController } from '@infra/http/controllers/BaseController';
 import { SignUpController } from '@infra/http/controllers/authentication/SignUpController';
 import { makeSignUpValidation } from '@main/factories/controllers/authentication/sign-up/validation-factory';
-import { makeSignUp } from '@main/factories/use-cases/authentication/sign-up-factory';
 import { makeSignIn } from '@main/factories/use-cases/authentication/sign-in-factory';
+import { makeSignUp } from '@main/factories/use-cases/authentication/sign-up-factory';
 
 export const makeSignUpController = (): BaseController => {
   const validation = makeSignUpValidation();
