f253612fe18a5904fc280fa0f92473eea82f4b09
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-04-26 12:20:35 +0000

    26 Apr 2024, 17:50

--- src/application/interfaces/cryptography/JWTGenerator.ts
+++ src/application/interfaces/cryptography/JWTGenerator.ts
@@ -1,3 +1,10 @@
+export interface JWTGeneratorOptions {
+  algorithm?: string;
+  expiresIn?: string;
+  issuer?: string;
+  jwtId?: string;
+}
+
 export interface JWTGenerator {
-  generate(payload: string): Promise<string>;
+  generate(payload: string, jwtOptions?: JWTGeneratorOptions): Promise<string>;
 }
